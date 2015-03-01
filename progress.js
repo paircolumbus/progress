d4082c4eb1d265178da5450e51ac7525921b4d28
Nicolas McCurdy <thenickperson@gmail.com>
2015-02-28 21:59:34 -0500

    Initial progress on showing challenge progress (with a hard coded user)

---
+++ progress.js
@@ -0,0 +1,54 @@
+var async = require('async');
+var request = require('request');
+
+function githubRequest(pathname, callback) {
+  request({
+    url: 'https://api.github.com' + pathname,
+    headers: {
+      Accept: 'application/vnd.github.v3',
+      'User-Agent': 'request'
+    },
+    json: true,
+    qs: {
+      client_id: process.env.PROGRESS_ID,
+      client_secret: process.env.PROGRESS_SECRET
+    }
+  }, function (error, response, body) {
+    if (body.message) {
+      callback(new Error(body.message), body);
+    } else {
+      callback(error, body);
+    }
+  });
+}
+
+function getProgressAll(callback) {
+  githubRequest('/orgs/paircolumbus/repos', function (error, repos) {
+    if (error || !repos) {
+      callback(error);
+    } else {
+      var challenges = repos.map(function (repo) { return repo.name; });
+      async.map(challenges, getProgress, callback);
+    }
+  });
+}
+
+function getProgress(challenge, callback) {
+  githubRequest('/repos/paircolumbus/' + challenge + '/pulls', function (error, body) {
+    if (error) {
+      callback(error);
+    } else {
+      callback(error, {
+        challenge: challenge,
+        complete: body.some(function (pull) {
+          return pull.user.login === 'nicolasmccurdy';
+        })
+      });
+    }
+  });
+}
+
+module.exports = {
+  getProgress: getProgress,
+  getProgressAll: getProgressAll
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var progress = require('../progress');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  progress.getProgressAll(function (error, results) {
+    if (error) {
+      next(error);
+    } else {
+      res.render('index', { results: results });
+    }
+  });
 });
 
 module.exports = router;
